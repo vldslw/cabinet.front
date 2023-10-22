1ae69540d8145ab59b1fabb5a8298557ff11a66a
Vladislav Pavlov <vldslw@gmail.com>
2023-10-22 19:01:41 -0300

    feat: add unload desc and info blocks"

--- pages/lk/manager/unload.vue
+++ pages/lk/manager/unload.vue
@@ -1,3 +1,29 @@
 <template>
-  <main>Страница выгрузки</main>
+  <main class="unload">
+    <section class="unload__left">
+      <div class="unload__desc block">
+          <h2 class="unload__desc-title">Выгрузка</h2>
+          <h3 class="unload__desc-subtitle"><span class="text-bold">Выполняет работу:</span></h3>
+          <ul class="unload__desc-list">
+            <li class="unload__desc-list-item"> - Собирает фотографии из заказов пользователей.</li>
+            <li class="unload__desc-list-item"> - Выгружает по папкам.</li>
+          </ul>
+        </div>
+    </section>
+    <section class="unload__right">
+      <div class="notice" data-color="light-purple">
+        <p>Для того, чтобы посмотреть информацию о <span class="text-bold">выгрузке</span>, а также ее скачать, нажмите на требуемую выгрузку в столбце слева</p>
+      </div>
+    </section>
+  </main>
 </template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
