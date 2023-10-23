fc7ee17e66080e4eacb997327ce5e19938dbc70a
Vladislav Pavlov <vldslw@gmail.com>
2023-10-23 01:49:01 -0300

    feat: add fetch download link

--- pages/lk/manager/unload.vue
+++ pages/lk/manager/unload.vue
@@ -9,7 +9,10 @@
           <li class="unload__desc-item"> - Выгружает по папкам.</li>
         </ul>
       </div>
-      <div v-for="card in cards" :key="card.id" class="block unload__card">
+      <div 
+        v-for="card in cards" 
+        :key="card.id" class="block unload__card" 
+        @click="fetchLink(card.id)">
         <p class="unload__card-text">Задача выполнена: <span class="text-bold">{{ card.date }}</span></p>
         <p class="unload__card-text">Статус задачи: <span class="text-bold">{{ card.status_text }}</span> </p>
         <p class="unload__card-text">ID выгрузки: <span class="text-bold">{{ card.id }}</span></p>
@@ -18,19 +21,46 @@
       </div>
     </section>
     <section class="unload__right">
-      <div class="notice unload__info" data-color="light-purple">
+      <div v-if="showInfo === true" class="notice unload__info" data-color="light-purple">
         <p>Для того, чтобы посмотреть информацию о <span class="text-bold">выгрузке</span>, а также ее скачать, нажмите на требуемую выгрузку в столбце слева</p>
+      </div>
+      <div v-else class="block unload__download">
+        <h2 class="unload__download-title"><span class="text-bold">Ссылка для скачивания архива Выгрузки (.zip):</span></h2>
+        <a class="link unload__download-link" :href="link">{{ link }}</a>
+        <button class="span-link unload__download-btn">cкопировать ссылку</button>
       </div>
     </section>
   </main>
 </template>
 
-<script setup>
-  const { data } = await useFetch(`https://dev-cabinet.seenday.com/e.scripts?page`, {
-    query: { page: 'pages:unload', event: 'get' }
-  });
-  const jsonData = await JSON.parse(data.value);
-  const cards = toRaw(jsonData.response.data);
+<script>
+  export default {
+    async setup() {
+    const { data } = await useFetch(`https://dev-cabinet.seenday.com/e.scripts?page`, {
+      query: { page: 'pages:unload', event: 'get' }
+    });
+    const jsonData = JSON.parse(data.value);
+    const cards = toRaw(jsonData.response.data);
+    return { cards };
+    },
+    data () {
+      return {
+        showInfo: true,
+        link: '',
+      }
+    },
+    methods: {
+      async fetchLink(id) {
+        const { data } = await useFetch(`https://dev-cabinet.seenday.com/e.scripts?page`, {
+          query: { page: 'pages:unload', event: 'get', unload_id: id }
+        });
+        const jsonData = JSON.parse(data.value);
+        const cardLink = toRaw(jsonData.response.data[0].download_link);
+        this.link = cardLink;
+        this.showInfo = false;
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
